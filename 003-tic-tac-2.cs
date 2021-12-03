1e6b8d079205fe4279d67e668a26161af551885a
n-kev1n <kevin_@tuta.io>
2021-12-03 20:26:30 +0100

    Improved a little

--- 003-tic-tac-2.cs
+++ 003-tic-tac-2.cs
@@ -28,61 +28,68 @@ namespace TicTacToe2
 			// Game's Main Loop...
 			do
 			{
+				// Checking if All Fields are full...
+				if (	(v1 == 'X' || v1 == 'O' ) &&
+						(v2 == 'X' || v2 == 'O' ) && 
+						(v3 == 'X' || v3 == 'O' ) && 
+						(v4 == 'X' || v4 == 'O' ) && 
+						(v5 == 'X' || v5 == 'O' ) && 
+						(v6 == 'X' || v6 == 'O' ) && 
+						(v7 == 'X' || v7 == 'O' ) && 
+						(v8 == 'X' || v8 == 'O' ) && 
+						(v9 == 'X' || v9 == 'O' ) 
+					)
+				{
+					flag = true;
+					Console.WriteLine("Game Over!");
+				}
 				// Getting Input from the User...
 				Console.WriteLine("X\'s Move > ");
 				userInput1 = Convert.ToInt32(Console.ReadLine());
-				Console.WriteLine("\nO\'s Move > ");
-				userInput2 = Convert.ToInt32(Console.ReadLine());
 				
+
 				// Validating Inputs...
-				switch (userInput1)
+				if (userInput1 == 1)
 				{
-					case 1:
-						v1 = GAMECHAR1;break;
-					case 2:
-						v2 = GAMECHAR1;break;
-					case 3:
-						v3 = GAMECHAR1;break;
-					case 4:
-						v4 = GAMECHAR1;break;
-					case 5:
-						v5 = GAMECHAR1;break;
-					case 6:
-						v6 = GAMECHAR1;break;
-					case 7:
-						v7 = GAMECHAR1;break;
-					case 8:
-						v8 = GAMECHAR1;break;
-					case 9:
-						v9 = GAMECHAR1;break;
-					default:
-						Console.WriteLine("Invalid Move");break;
+					v1 = GAMECHAR1;
 				}
-
-				switch (userInput2)
+				else if (v2 == ' ' && userInput1 == 2)
 				{
-					case 1:
-						v1 = GAMECHAR2;break;
-					case 2:
-						v2 = GAMECHAR2;break;
-					case 3:
-						v3 = GAMECHAR2;break;
-					case 4:
-						v4 = GAMECHAR2;break;
-					case 5:
-						v5 = GAMECHAR2;break;
-					case 6:
-						v6 = GAMECHAR2;break;
-					case 7:
-						v7 = GAMECHAR2;break;
-					case 8:
-						v8 = GAMECHAR2;break;
-					case 9:
-						v9 = GAMECHAR2;break;
-					default:
-						Console.WriteLine("Invalid Move");break;
+					v2 = GAMECHAR1;
 				}
-
+				else if (v3 == ' ' && userInput1 == 3)
+				{
+					v3 = GAMECHAR1;
+				}
+				else if (v4 == ' ' && userInput1 == 4)
+				{
+					v4 = GAMECHAR1;
+				}
+				else if (v5 == ' ' && userInput1 == 5)
+				{
+					v5 = GAMECHAR1;
+				}
+				else if (v6 == ' ' && userInput1 == 6)
+				{
+					v6 = GAMECHAR1;
+				}
+				else if (v7 == ' ' && userInput1 == 7)
+				{
+					v7 = GAMECHAR1;
+				}
+				else if (v8 == ' ' && userInput1 == 8)
+				{
+					v8 = GAMECHAR1;
+				}
+				else if (v9 == ' ' && userInput1 == 9)
+				{
+					v9 = GAMECHAR1;
+				}
+				else
+				{
+					Console.WriteLine("Invalid Move!");
+					break;
+				}
 
 				// Drawing the Board...
 				Console.WriteLine("\n     " + v1 + " | " + v2 + " | " + v3 + "\n    ---+---+--- " +
@@ -90,7 +97,56 @@ namespace TicTacToe2
 								  "\n     " + v7 + " | " + v8 + " | " + v9 + "\n\n"
 					  			 );
 
-				flag = true;
+				Console.WriteLine("\nO\'s Move > ");
+				userInput2 = Convert.ToInt32(Console.ReadLine());
+
+				// Validating Inputs...
+				if (v1 == ' ' && userInput2 == 1)
+				{
+					v1 = GAMECHAR2;
+				}
+				else if (v2 == ' ' && userInput2 == 2)
+				{
+					v2 = GAMECHAR2;
+				}
+				else if (v3 == ' ' && userInput2 == 3)
+				{
+					v3 = GAMECHAR2;
+				}
+				else if (v4 == ' ' && userInput2 == 4)
+				{
+					v4 = GAMECHAR2;
+				}
+				else if (v5 == ' ' && userInput2 == 5)
+				{
+					v5 = GAMECHAR2;
+				}
+				else if (v6 == ' ' && userInput2 == 6)
+				{
+					v6 = GAMECHAR2;
+				}
+				else if (v7 == ' ' && userInput2 == 7)
+				{
+					v7 = GAMECHAR2;
+				}
+				else if (v8 == ' ' && userInput2 == 8)
+				{
+					v8 = GAMECHAR2;
+				}
+				else if (v9 == ' ' && userInput2 == 9)
+				{
+					v9 = GAMECHAR2;
+				}
+				else
+				{
+					Console.WriteLine("Invalid Move!");
+					break;
+				}
+				
+				Console.WriteLine("\n     " + v1 + " | " + v2 + " | " + v3 + "\n    ---+---+--- " +
+								  "\n     " + v4 + " | " + v5 + " | " + v6 + "\n    ---+---+--- " +
+								  "\n     " + v7 + " | " + v8 + " | " + v9 + "\n\n"
+					  			 );
 			}
 			while(!flag);
 		}
