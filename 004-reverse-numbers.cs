9b930eeccf93b4550609e234091afd731854ebee
n-kev1n <kevin_@tuta.io>
2021-12-09 12:56:05 +0100

    Added Exception Handling for user input

--- 004-reverse-numbers.cs
+++ 004-reverse-numbers.cs
@@ -11,16 +11,17 @@ namespace Program
 			Console.WriteLine("\nEnter 20 Numbers:\n");
 			
 			// Loop to Get Numbers...
-			for (int i = 0; i <= 19; i++)
+			for (int i = 0; i < Numbers.Length; i++)
 			{
-				Console.Write("> ");
-				Numbers[i] = Convert.ToInt32(Console.ReadLine());
+				Console.Write($"{i}> ");
+				// Numbers[i] = Convert.ToInt32(Console.ReadLine());
+				while(!int.TryParse(Console.ReadLine(), out Numbers[i]));
 			}
 			
 			Console.WriteLine("Reversed Numbers");
 
 			// Loop to Print Numbers...
-			for (int j = 19; j >= 0; j--)
+			for (int j = Numbers.Length - 1; j >= 0; j--)
 			{
 				Console.Write($"{Numbers[j]} ");
 			}
