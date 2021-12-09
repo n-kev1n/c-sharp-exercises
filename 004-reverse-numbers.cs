9191558c3328ec464612c950b76c2c87513b1f9f
n-kev1n <kevin_@tuta.io>
2021-12-09 12:58:32 +0100

    Reverse Numbers in Arrays

--- 004-reverse-numbers.cs
+++ 004-reverse-numbers.cs
@@ -13,7 +13,7 @@ namespace Program
 			// Loop to Get Numbers...
 			for (int i = 0; i < Numbers.Length; i++)
 			{
-				Console.Write($"{i}> ");
+				Console.Write($"{i + 1}> ");
 				// Numbers[i] = Convert.ToInt32(Console.ReadLine());
 				while(!int.TryParse(Console.ReadLine(), out Numbers[i]));
 			}
