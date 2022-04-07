7663ac091165b27e5ca46a183b0147ffc19624b9
n-kev1n <kevin_@tuta.io>
2022-04-07 15:57:55 +0200

    added a better solution

--- task-1/summation.cs
+++ task-1/summation.cs
@@ -4,9 +4,11 @@ public static class Kata
 {
     public static int summation(int num)
     {
-      int sum = 0;
-      for(int i = 1; i <= num; i++)
-        sum = sum + i;
-      return sum;
+      return num*(num+1)/2;
+      /* int sum = 0; 
+         for(int i = 1; i <= num; i++) 
+           sum = sum + i;
+         return sum; 
+       */
     }
 }
