1a9fc3ba0bea393de3e8a97ba0c75cffcb5167ed
n-kev1n <kevin_@tuta.io>
2022-04-09 15:18:02 +0200

    finished solution for #2

---
+++ task-2/solution.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    static void Main(string[] args)
+    {
+        /* int[] arr = {12,1,-2,-4}; */
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+        int[] solution = CountPositivesSumNegatives(arr);
+        Console.WriteLine($"{solution[0]} {solution[1]}");
+    }
+
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        int[] arr = {0,0};
+        if(input == null || !input.Any())  return new int[] {};
+        for(int i = 0; i < input.Length; i++)
+        {
+           if(input[i] > 0){
+             arr[0] = arr[0] + 1;
+            }
+           else if(input[i] < 0){
+             arr[1] = arr[1] + input[i];
+            }
+        }
+        return arr;
+    }
+}
