854ed5db9df7c6df27f126372a7572ed66c18570
n-kev1n <kevin_@tuta.io>
2022-04-09 15:21:47 +0200

    added better solution

--- task-2/solution.cs
+++ task-2/solution.cs
@@ -6,7 +6,6 @@ public class Kata
 {
     static void Main(string[] args)
     {
-        /* int[] arr = {12,1,-2,-4}; */
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
         int[] solution = CountPositivesSumNegatives(arr);
         Console.WriteLine($"{solution[0]} {solution[1]}");
@@ -14,17 +13,21 @@ public class Kata
 
     public static int[] CountPositivesSumNegatives(int[] input)
     {
-        int[] arr = {0,0};
+        /* int[] arr = {0,0}; */
         if(input == null || !input.Any())  return new int[] {};
-        for(int i = 0; i < input.Length; i++)
-        {
-           if(input[i] > 0){
-             arr[0] = arr[0] + 1;
-            }
-           else if(input[i] < 0){
-             arr[1] = arr[1] + input[i];
-            }
-        }
-        return arr;
+        int countPositives = input.Count(n => n > 0);
+        int sumNegatives = input.Where(n => n < 0).Sum();
+
+        return new int[] { countPositives, sumNegatives };
+        /* for(int i = 0; i < input.Length; i++) */
+        /* { */
+        /*    if(input[i] > 0){ */
+        /*      arr[0] = arr[0] + 1; */
+        /*     } */
+        /*    else if(input[i] < 0){ */
+        /*      arr[1] = arr[1] + input[i]; */
+        /*     } */
+        /* } */
+        /* return arr; */
     }
 }
